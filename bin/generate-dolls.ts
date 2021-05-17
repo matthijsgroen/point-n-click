8c439508174b85b1bbecb78fe16d45e4a21e91ea
Matthijs Groen <matthijs.groen@gmail.com>
2021-05-17 20:56:33 +0200

    Generate doll index files

--- bin/generate-dolls.ts
+++ bin/generate-dolls.ts
@@ -1,7 +1,9 @@
-import { readdir as readdirCb } from "fs";
+import { readdir as readdirCb, writeFile as writeFileCb } from "fs";
 import { promisify } from "util";
 import { join, basename, extname } from "path";
 const readdir = promisify(readdirCb);
+const writefile = promisify(writeFileCb);
+import { format } from "prettier";
 
 const getImagesFromLayer = async (path: string) =>
   (await readdir(path, { withFileTypes: true }))
@@ -16,22 +18,64 @@ const buildCharacterStructure = async (path: string, name: string) => {
     .map((e) => e.name)
     .sort();
   const layersNames = layers.map((e) => e.split("_").slice(1).join("_"));
-  const images = {};
+  const images: Record<string, Record<string, string>> = {};
 
   for (const index in layers) {
     const imageNames = await getImagesFromLayer(join(path, layers[index]));
     images[layersNames[index]] = imageNames.reduce<Record<string, string>>(
       (result, name) => ({
         ...result,
-        [basename(name, extname(name))]: join(path, layers[index], name),
+        [basename(name, extname(name))]: join(layers[index], name),
       }),
       {}
     );
-
-    // console.log(layersNames[index], layers[index], images);
   }
 
   return { name, layers: layersNames, images };
+};
+
+const createCharacterFile = async (
+  characterPath: string,
+  folderName: string
+) => {
+  const data = await buildCharacterStructure(characterPath, folderName);
+  let inputCounter = 1;
+
+  const imports = [`import { Doll } from "../../../types/Doll";`];
+
+  const generateImageLists = (layer: string): string[] => {
+    const itemKeys = Object.keys(data.images[layer]);
+    const result = [`    "${layer}": {`];
+    for (const key of itemKeys) {
+      imports.push(
+        `import img${inputCounter} from "url:./${data.images[layer][key]}";`
+      );
+      result.push(`"${key}": img${inputCounter},`);
+      inputCounter++;
+    }
+    result.push("},");
+    return result;
+  };
+
+  const generatedCode = [
+    "",
+    `const ${folderName}: Doll = {`,
+    `name: "${folderName}",`,
+    `size: [600, 800],`, // Read from disk?
+    'type: "imagestack",',
+    `layers: ["${data.layers.join('", "')}"],`,
+    `images: {`,
+    ...data.layers.reduce((r, e) => r.concat(generateImageLists(e)), []),
+    "}",
+    "};",
+    "",
+    `export default ${folderName};`,
+  ];
+
+  const indexFileCode = [...imports, ...generatedCode].join("\n");
+  const formatted = format(indexFileCode, { parser: "typescript" });
+
+  writefile(join(characterPath, "index.ts"), formatted);
 };
 
 const program = async () => {
@@ -42,12 +86,10 @@ const program = async () => {
 
   const folders = entries.filter((e) => e.isDirectory()).map((e) => e.name);
 
-  const items = await Promise.all(
+  await Promise.all(
     folders.map((folder) =>
-      buildCharacterStructure(join(dollContentPath, folder), folder)
+      createCharacterFile(join(dollContentPath, folder), folder)
     )
   );
-
-  console.log(JSON.stringify(items, null, 2));
 };
 program();
