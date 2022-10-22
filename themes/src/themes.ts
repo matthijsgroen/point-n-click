332ed350a4ee3033b133c77cbead7c6053024263
Matthijs Groen <matthijs.groen@gmail.com>
2022-10-22 11:16:06 +0200

    Create themes api package

---
+++ themes/src/themes.ts
@@ -0,0 +1,25 @@
+import { GameWorld } from "@point-n-click/types";
+import type { FC } from "react";
+import {
+  DisplayInfo,
+  GameModelManager,
+  Interactions,
+} from "@point-n-click/engine";
+
+export type ThemeRenderer<Settings extends Record<string, unknown>> = FC<{
+  settings: Settings;
+  contents: DisplayInfo<GameWorld>[];
+  interactions: Interactions;
+  gameModelManager: GameModelManager<GameWorld>;
+  onInteraction: (interactionId: string) => void;
+}>;
+
+export type Theme<Settings extends Record<string, unknown>> = {
+  name: string;
+  version: string;
+  author: string;
+  package: string;
+  render: ThemeRenderer<Settings>;
+  defaultSettings: Settings;
+  // Will be extended with other render functions, like menu's
+};
