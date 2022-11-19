630c3dd3cb44abace066ca03885a1954681a6f80
Matthijs Groen <matthijs.groen@gmail.com>
2022-11-19 17:28:00 +0100

    First version of first content plugin

---
+++ packages/content-description-text/src/index.ts
@@ -0,0 +1,39 @@
+import {
+  ContentPlugin,
+  ContentStatement,
+  SystemInterface,
+} from "@point-n-click/types";
+
+type TextStatement = {
+  statementType: "descriptionText";
+  content: string[];
+};
+
+const isTextStatement = (item: ContentStatement): item is TextStatement =>
+  item.statementType === "descriptionText";
+
+const textDslFunctions = {
+  descriptionText: (system: SystemInterface, ...sentences: string[]) => {
+    system.addContent({ statementType: "descriptionText", content: sentences });
+  },
+} as const;
+
+const textContent: ContentPlugin<typeof textDslFunctions> = {
+  pluginType: "descriptionText",
+  dslFunctions: textDslFunctions,
+  translationContent: (content) => {
+    const translationScope = {
+      descriptionText: {},
+    };
+    for (const statement of content) {
+      if (isTextStatement(statement)) {
+        for (const sentence of statement.content) {
+          translationScope.descriptionText[sentence] = sentence;
+        }
+      }
+    }
+    return translationScope;
+  },
+};
+
+export default textContent;
